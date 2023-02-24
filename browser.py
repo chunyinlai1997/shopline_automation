b13eda4f1683849c195db976598b02f9db1f2818
chunyinlai1997 <prologic33@gmail.com>
2023-02-24 17:39:08 +0800

    version 2

--- browser.py
+++ browser.py
@@ -3,7 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import Keys, ActionChains
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import xlrd
+import json
 
 class Action():
     def __init__(self) -> None:
@@ -89,7 +92,7 @@ class Action():
         driver.find_element(By.ID, "reg-submit-button").click()
         print("Login sucessful")
         
-        for sku_id, bar, has_varient, period_type in process_list:
+        for sku_id, has_varient, period_type in process_list:
             print("Now browsing to SKU: " + sku_id)
             driver.get("https://admin.shoplineapp.com/admin/waddystore/products/"+sku_id+"/edit")
             driver.implicitly_wait(10)
@@ -109,8 +112,8 @@ class Action():
                     driver.find_element(By.XPATH,'//*[@id="product_form"]/div[1]/div[3]/ul/li[5]/a').click()
                     self.pre_order_button_handler(driver,xpath,"open")
 
-            
-            driver.find_element(By.XPATH,'//*[@id="product_form"]/div[1]/div[3]/ul/li[8]/a').click()
+            element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="product_form"]/div[1]/div[3]/ul/li[8]/a')))
+            element.click()
             print("Go to Settings Tab")
             pre_order_switch = driver.find_element(By.XPATH, '//*[@id="productForm-settings"]/div[1]/div[3]/div[1]/div/div[2]/div/div[1]/div')
             pre_order_switch_classess = pre_order_switch.get_attribute("class")
@@ -150,7 +153,7 @@ class Action():
         driver.find_element(By.ID, "reg-submit-button").click()
         print("Login sucessful")
 
-        for sku_id, bar, has_varient in process_list:
+        for sku_id, has_varient in process_list:
             print("Now browsing to SKU: " + sku_id)
             driver.get("https://admin.shoplineapp.com/admin/waddystore/products/"+sku_id+"/edit")
             driver.implicitly_wait(15)
@@ -171,8 +174,8 @@ class Action():
                     driver.find_element(By.XPATH,'//*[@id="product_form"]/div[1]/div[3]/ul/li[5]/a').click()
                     self.pre_order_button_handler(driver,xpath,"close")
 
-        
-            driver.find_element(By.XPATH,'//*[@id="product_form"]/div[1]/div[3]/ul/li[8]/a').click()
+            element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="product_form"]/div[1]/div[3]/ul/li[8]/a')))
+            element.click()
             print("Go to Settings Tab")
 
             pre_order_switch = driver.find_element(By.XPATH, '//*[@id="productForm-settings"]/div[1]/div[3]/div[1]/div/div[2]/div/div[1]/div')
@@ -185,4 +188,83 @@ class Action():
 
             driver.find_element(By.XPATH,'//*[@id="product_form"]/div[1]/div[1]/div/span[2]/button/span[2]').click()
             print("Saved changes, completed")
+
+    def close_preorder_by_keywords(self):
+        driver = webdriver.Chrome()
+
+        username = "info@waddystore.com"
+        password = "Waddy1208"
+        driver.get('https://admin.shoplineapp.com/admin/waddystore/')
+        driver.find_element(By.ID, "staff_email").send_keys(username)
+        driver.find_element(By.ID, "staff_password").send_keys(password)
+        driver.find_element(By.ID, "reg-submit-button").click()
+        print("Login Shopline Admin successful")
+
+        driver.get('https://admin.shoplineapp.com/api/admin/v1/5f23e6c55680fc0012f13584/products?page=1&offset=0&limit=1000&scope=preorder')
+        html_response = driver.find_element(By.XPATH, '/html/body/pre').text
+        json_data = json.loads(html_response)
+
+        product_items = json_data['data']['items']
+        print("total items found: " + str( len(product_items)))
+        keyword = input("Please input the keywords or exact product Chinese name: ")
+        process_list = []
+
+        # Loop through each item in the JSON data and check if the keyword matches any value in the "title_translations" dictionary
+        for item in product_items:
+            chinese_name = item['title_translations']['zh-hant']
+            sku_id = item['id']
+            has_varient = False
+            print()
+            if len(item["variations"]) > 0:
+                has_varient = True
+            
+            if keyword in chinese_name:
+                print(chinese_name)
+                process_list.append([sku_id, has_varient])
+
+        print("total items execute: " + str( len(process_list)))
+        self.close_preorder(process_list) 
+        print("Task Complete")
+
+    def find_missing_allow_oversale(self):
+        driver = webdriver.Chrome()
+
+        username = "info@waddystore.com"
+        password = "Waddy1208"
+        driver.get('https://admin.shoplineapp.com/admin/waddystore/')
+        driver.find_element(By.ID, "staff_email").send_keys(username)
+        driver.find_element(By.ID, "staff_password").send_keys(password)
+        driver.find_element(By.ID, "reg-submit-button").click()
+        print("Login Shopline Admin successful")
+
+        keyword = input("Please input the keywords or exact product Chinese name: ")
+        driver.get('https://admin.shoplineapp.com/api/admin/v1/5f23e6c55680fc0012f13584/products?page=1&offset=0&limit=10000&query='+ keyword+'&scope=search')
+        html_response = driver.find_element(By.XPATH, '/html/body/pre').text
+        json_data = json.loads(html_response)
+
+        product_items = json_data['data']['items']
+        print("total items found: " + str( len(product_items)))
         
+        process_list = []
+
+        data = self.xls_to_list('search/namelist.xls')
+        data.pop(0)
+        search_for = dict([[row[0], row[1]] for row in data])
+
+        for item in product_items:
+            chinese_name = item['title_translations']['zh-hant']
+            sku_id = item['id']
+            has_varient = False
+            if len(item["variations"]) > 0:
+                has_varient = True
+            
+            if keyword in chinese_name:
+                if any(key in chinese_name for key in search_for.keys()):
+                    print(chinese_name + " in pre-order keyword list, skip")
+                else:
+                    print(chinese_name)
+                    process_list.append([sku_id, has_varient])
+
+        print("total items execute: " + str( len(process_list)))
+        self.close_preorder(process_list) 
+        print("Task Complete")
--- preorder.py
+++ preorder.py
@@ -1,62 +1,169 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import browser
-import inv_dataset
-import os
-import pandas as pd
+import json
 
 class Preorder():
 
     def __init__(self) -> None:
         pass
-    
-    def process_update_data (self, inventory_folder_path):
-        inventory_files = inv_dataset.Converter()
-        inventory_files.to_csv(inventory_folder_path)
 
-        inventory = inv_dataset.Reader()
-        inv_df = inventory.get_df(inventory_folder_path)
- 
-        return inv_df
-
-    def PreOrderClose(self, udpate_folder_path, inventory_folder_path):   
-        print(os.getcwd())
-
-        inv_df = self.process_update_data(inventory_folder_path)
-
+    #following is the old version
+    def PreOrderClose(self):
+        driver = webdriver.Chrome()
         clicker = browser.Action()
         process_list = []
+
+        username = "info@waddystore.com"
+        password = "Waddy1208"
+        driver.get('https://admin.shoplineapp.com/admin/waddystore/')
+        driver.find_element(By.ID, "staff_email").send_keys(username)
+        driver.find_element(By.ID, "staff_password").send_keys(password)
+        driver.find_element(By.ID, "reg-submit-button").click()
+        print("Login Shopline Admin successful")
+
+        driver.get('https://admin.shoplineapp.com/api/admin/v1/5f23e6c55680fc0012f13584/products?page=1&offset=0&limit=1000&scope=preorder')
+        html_response = driver.find_element(By.XPATH, '/html/body/pre').text
+        json_data = json.loads(html_response)
+
+        product_items = json_data['data']['items']
+        print("total items found: " + str( len(product_items)))
         
-        update_files = inv_dataset.Converter()
-        update_files.to_csv(udpate_folder_path)
+        for item in product_items:
+            quantity = item['quantity']
+            status = item['status']
+            is_preorder = item['is_preorder']
+            sku_id = item['id']
+            chinese_name = item['title_translations']['zh-hant']
+            has_varient = False
+            if len(item["variations"]) > 0:
+                has_varient = True
 
-        #Start update procedure
-        os.chdir(udpate_folder_path)
-        
-        #go thru each bulk update form csv
-        for file in os.listdir():
-            if file.endswith(".csv"):
-                #in each bulk update form csv
-                file_path = f"{file}"
-                df = pd.read_csv(file_path, index_col=None, header=0)
-                df = df.drop(index=0)
+            if int(quantity) > 0 and status == "active" and is_preorder == True:
+                print(chinese_name)
+                process_list.append([sku_id, has_varient])
 
-                df = df.loc[df['Preorder Product'] == "Y"]
-                df = df.loc[df['Status'] == "Y"]
-                
-                for barcode in df['Barcode']:
-                    df2 = inv_df.loc[inv_df['商品條碼'] == barcode]
-                    df2 = df2.loc[df2['預設倉庫'] > 0]
-                    for bar in df2['商品條碼']:
-                        sku_id = df[df['Barcode']== bar]['Product ID (DO NOT EDIT)'].item()
-                        varient_id = df[df['Barcode']== bar]['Variant ID (DO NOT EDIT)'].item()
-                        has_varient = True
-                        if pd.isnull(varient_id):
-                            has_varient = False
-                        process_list.append([sku_id, bar, has_varient])
-
-        os.chdir("../..") 
+        print("Process items: ")
         print(process_list) 
         clicker.close_preorder(process_list)   
-        print("All Completed, End Task.")    
+        print("All Completed, End Task.")
+
+    def PreOrderOpen(self):
+        driver = webdriver.Chrome()
+        clicker = browser.Action()
+        process_list = []
+
+        username = "info@waddystore.com"
+        password = "Waddy1208"
+        driver.get('https://admin.shoplineapp.com/admin/waddystore/')
+        driver.find_element(By.ID, "staff_email").send_keys(username)
+        driver.find_element(By.ID, "staff_password").send_keys(password)
+        driver.find_element(By.ID, "reg-submit-button").click()
+        print("Login Shopline Admin successful")
+
+        data = clicker.xls_to_list('search/namelist.xls')
+        data.pop(0)
+        search_for = dict([[row[0], row[1]] for row in data])
+
+        exclude_list = []
+        while True:
+            get_exclude = input("Any product you want to exclude? Please type in one by one (if no more please type 'N') : ")
+            if get_exclude.lower() == 'n':
+                break
+            else: 
+                exclude_list.append(get_exclude)
+
+        print("Your exclude list:")
+        print(exclude_list)
+        print("items found: ")
+        
+        for key in search_for.keys():
+            driver.get('https://admin.shoplineapp.com/api/admin/v1/5f23e6c55680fc0012f13584/products?page=1&offset=0&limit=10000&query='+ key +'&scope=search')
+            html_response = driver.find_element(By.XPATH, '/html/body/pre').text
+            json_data = json.loads(html_response)
+
+            product_items = json_data['data']['items']
+
+            for item in product_items:
+                quantity = item['quantity']
+                status = item['status']
+                is_preorder = item['is_preorder']
+                sku_id = item['id']
+                chinese_name = item['title_translations']['zh-hant']
+                has_varient = False
+                if len(item["variations"]) > 0:
+                    has_varient = True
+
+                not_dis = False
+
+                if item['tags_array'] is None:
+                    not_dis = True
+                elif 'dis' not in item['tags_array'] or 'dis' not in item['sku']:
+                    not_dis = True
+                
+                if not_dis == True and chinese_name not in exclude_list:
+                    if quantity <= 0 and not is_preorder and status == "active":
+                        print(chinese_name)
+                        process_list.append([sku_id, has_varient, search_for[key]])
+
+        print("Process items: ")
+        print(process_list) 
+        clicker.open_preorder(process_list)   
+        print("All Completed, End Task.")
+
+    
+
+'''
+def process_update_data (self, inventory_folder_path):
+    inventory_files = inv_dataset.Converter()
+    inventory_files.to_csv(inventory_folder_path)
+
+    inventory = inv_dataset.Reader()
+    inv_df = inventory.get_df(inventory_folder_path)
+
+    return inv_df
+
+#following is the old version
+def PreOrderClose(self, udpate_folder_path, inventory_folder_path):   
+    print(os.getcwd())
+
+    inv_df = self.process_update_data(inventory_folder_path)
+
+    clicker = browser.Action()
+    process_list = []
+    
+    update_files = inv_dataset.Converter()
+    update_files.to_csv(udpate_folder_path)
+
+    #Start update procedure
+    os.chdir(udpate_folder_path)
+    
+    #go thru each bulk update form csv
+    for file in os.listdir():
+        if file.endswith(".csv"):
+            #in each bulk update form csv
+            file_path = f"{file}"
+            df = pd.read_csv(file_path, index_col=None, header=0)
+            df = df.drop(index=0)
+
+            df = df.loc[df['Preorder Product'] == "Y"]
+            df = df.loc[df['Status'] == "Y"]
+            
+            for barcode in df['Barcode']:
+                df2 = inv_df.loc[inv_df['商品條碼'] == barcode]
+                df2 = df2.loc[df2['預設倉庫'] > 0]
+                for bar in df2['商品條碼']:
+                    sku_id = df[df['Barcode']== bar]['Product ID (DO NOT EDIT)'].item()
+                    varient_id = df[df['Barcode']== bar]['Variant ID (DO NOT EDIT)'].item()
+                    has_varient = True
+                    if pd.isnull(varient_id):
+                        has_varient = False
+                    process_list.append([sku_id, has_varient])
+
+    os.chdir("../..") 
+    print(process_list) 
+    clicker.close_preorder(process_list)   
+    print("All Completed, End Task.")    
 
 
     def PreOrderOpen(self, udpate_folder_path, inventory_folder_path):
@@ -112,9 +219,12 @@ class Preorder():
                         if pd.isnull(varient_id):
                             has_varient = False
                         
-                        process_list.append([sku_id, bar, has_varient, period_type])
+                        process_list.append([sku_id, has_varient, period_type])
 
         print(process_list) 
         os.chdir("../..") 
         clicker.open_preorder(process_list)   
         print("All Completed, End Task.")    
+
+
+'''
