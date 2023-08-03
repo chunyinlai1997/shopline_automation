0c9c52ef0b4a0ffa4f1eb8cb552a25f79d88d214
Willon <prologic338@gmail.com>
2023-08-03 15:35:25 +0800

    multiple processing update

--- google_merchant_commpliance.py
+++ google_merchant_commpliance.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+import multiprocessing
 import json
 import time
 import csv
@@ -48,20 +49,23 @@ class Google_Category_Clicker():
             # Wait for the new page to load
             wait.until(EC.staleness_of(driver.find_element_by_tag_name('html')))
     
+    
     def csv_to_list(self, sourcefile):
         # Initialize an empty list to store the extracted values
         extracted_values = []
 
         # Read the CSV file
-        with open(sourcefile, 'r',  encoding='utf-8', errors='ignore') as csvfile:
+        with open(sourcefile, 'r', encoding='utf-8', errors='ignore') as csvfile:
             reader = csv.reader(csvfile)
             # Skip the header row (if present)
             next(reader)
-            # Extract the first column items and add to the list
+            # Extract the desired columns and add to the list
             for row in reader:
                 first_column = row[0]
-                extracted_value = first_column.split(':')[0]
-                extracted_values.append(extracted_value)
+                third_column = row[2]
+                fourth_column = row[3]
+                fifth_column = row[4]
+                extracted_values.append([first_column, third_column, fourth_column, fifth_column])
 
         # Print the extracted values list
         print("Items found:", len(extracted_values))
@@ -95,7 +99,8 @@ class Google_Category_Clicker():
                 print("NoSuchElementException")
                 tab_found = False
 
-    def product_dropdown_handler(self, driver):
+    def product_dropdown_handler(self, driver, google_product_category, google_feed_options, google_feed_3rdlayer_options):
+        print("Google Category:", google_product_category, google_feed_options, google_feed_3rdlayer_options)
         #agegroup
         dropdown_element_agegroup = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div[1]/div[1]/div/div[2]/form/div[1]/div[3]/div/product-form-feed/div[4]/div[2]/div[1]/div[2]/div/select")
         dropdown_agegroup = Select(dropdown_element_agegroup)
@@ -109,32 +114,51 @@ class Google_Category_Clicker():
         #google_product_category
         dropdown_element_google_product_category = driver.find_element(By.ID, "google_feed_categories")
         dropdown_google_product_category = Select(dropdown_element_google_product_category)
-        dropdown_google_product_category.select_by_value("string:mature")
+        dropdown_google_product_category.select_by_value(google_product_category)
 
         #google_feed_options
         dropdown_element_google_feed_options = driver.find_element(By.ID, "google_feed_options")
         dropdown_google_feed_options = Select(dropdown_element_google_feed_options)
-        dropdown_google_feed_options.select_by_value("string:erotic")
+        dropdown_google_feed_options.select_by_value(google_feed_options)
 
         #google_feed_3rdlayer_options
         dropdown_element_google_feed_3rdlayer_options = driver.find_element(By.ID, "google_feed_3rdlayer_options")
         dropdown_google_feed_3rdlayer_options = Select(dropdown_element_google_feed_3rdlayer_options)
-        dropdown_google_feed_3rdlayer_options.select_by_value("string:sex_toys")
+        dropdown_google_feed_3rdlayer_options.select_by_value(google_feed_3rdlayer_options)
         time.sleep(1)
 
     def product_data(self, sku_id, driver):
         driver.get('https://admin.shoplineapp.com/api/admin/v1/5f23e6c55680fc0012f13584/products/'+ sku_id)
         html_response = driver.find_element(By.XPATH, '/html/body/pre').text
         json_data = json.loads(html_response)
-
         return True
 
     def UpdateGoogleCategorySexToy(self, process_list):
+        num_processes = 1
+        # Split process_list into smaller chunks for each process
+        chunk_size = len(process_list) // num_processes
+        process_chunks = [process_list[i:i + chunk_size] for i in range(0, len(process_list), chunk_size)]
+
+        # Create a list to store the processes
+        processes = []
+
+        # Create and start each process
+        for chunk in process_chunks:
+            process = multiprocessing.Process(target=self.process_chunk, args=(chunk,))
+            process.start()
+            processes.append(process)
+
+        # Wait for all processes to finish
+        for process in processes:
+            process.join()
+        
+    def process_chunk(self, sub_process_list):
+        print("Processing chuck items:", len(sub_process_list))
         driver = webdriver.Chrome()
 
         self.shopline_login(driver)
-        
-        for sku_id in process_list:
+
+        for sku_id, cat1, cat2, cat3 in sub_process_list:
             time.sleep(2)
             print("Now Processing:", sku_id)
             driver.get("https://admin.shoplineapp.com/admin/waddystore/products/"+sku_id+"/edit")
@@ -145,7 +169,7 @@ class Google_Category_Clicker():
                 print("Go to Product Feed Data Tab")
                 self.product_feed_data_button_handler(driver)
                 print("Change Dropdown Element Values")
-                self.product_dropdown_handler(driver)
+                self.product_dropdown_handler(driver, cat1, cat2, cat3)
                 #Save after changes
                 product_save_button.click()
                 print("Saved changes, completed")
@@ -156,12 +180,15 @@ class Google_Category_Clicker():
                 print("Go to Product Set Feed Data Tab")
                 self.product_set_feed_data_button_handler(driver)
                 print("Change Dropdown Element Values")
-                self.product_dropdown_handler(driver)
+                self.product_dropdown_handler(driver, cat1, cat2, cat3)
                 #Save after changes
                 button_xpath = '/html/body/div[3]/div[2]/div[1]/div[1]/div/div[2]/form/div[1]/div[1]/div/span[2]/button'
                 driver.find_element(By.XPATH, button_xpath).click()
                 print("Saved changes, completed")     
+        print("Process Chuck Completed") 
 
-google_category_clicks = Google_Category_Clicker()
-item_list = google_category_clicks.csv_to_list('item_issues.csv')
-google_category_clicks.UpdateGoogleCategorySexToy(item_list)
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    google_category_clicks = Google_Category_Clicker()
+    item_list = google_category_clicks.csv_to_list('issue_google_product_sets.csv')
+    google_category_clicks.UpdateGoogleCategorySexToy(item_list)
